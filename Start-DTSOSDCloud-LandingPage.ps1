813edf0698281892615e50693be8a1e983505f69
carlbarrettuk <43119941+carlbarrettuk@users.noreply.github.com>
2026-01-08 13:58:13 +0000

    Update Start-DTSOSDCloud-LandingPage.ps1
    25H2 test

--- Start-DTSOSDCloud-LandingPage.ps1
+++ Start-DTSOSDCloud-LandingPage.ps1
@@ -75,7 +75,7 @@ Write-Host ".................................................." -ForegroundColor
 
 switch ($EngineerInput)
 {
-    '1' { Start-OSDCloud -OSName "Windows 11 24H2 x64" -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage }
+    '1' { Start-OSDCloud -OSName "Windows 11 25H2 x64" -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage }
     '2' { Start-OSDCloud -OSName "Windows 11 24H2 x64" -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage da-DK }
     '3' { Start-OSDCloud -OSName "Windows 11 24H2 x64" -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage de-DE }
     '4' { Start-OSDCloud -OSName "Windows 11 24H2 x64" -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage hu-HU }
@@ -84,6 +84,7 @@ switch ($EngineerInput)
 }
 
 wpeutil reboot
+
 
 
 
